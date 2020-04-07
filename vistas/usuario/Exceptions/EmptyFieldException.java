c0462882ef6a0523fc2d77dfee2d9f5e3e411f18
HttpKiwi <juan.avenia@correounivalle.edu.co>
2020-04-06 20:36:45 -0500

    Adding EmptyFieldException.java

---
+++ vistas/usuario/Exceptions/EmptyFieldException.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class EmptyFieldException extends Exception
+    {
+        public EmptyFieldException(String msj)
+        {
+            super(msj);
+        }
+
+    }
+
+
