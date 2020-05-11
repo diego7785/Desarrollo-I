60ec4f6ef425ff996ecaddc4bc0fa2aeb7038ad0
johan <florez.johan@correounivalle.edu.co>
2020-05-11 00:52:11 -0500

    Improve readability

--- functionalities/administrador/Bill/src/CreateBill.java
+++ functionalities/administrador/Bill/src/CreateBill.java
@@ -16,25 +16,20 @@ public class CreateBill {
         String address = "";
         File directorio;
 
-        if(electronicBill)
-        {
+        if(electronicBill) {
             address = info[32];
             directorio = new File("./Bills/Bills sended by email/"+actual_year+"/"+month);
         }
-        else
-        {
+        else {
             address = info[30]+ " "+info[31];
             directorio = new File("./Bills/Bills to send home/"+actual_year+"/"+month);
         }
 
-        if (!directorio.exists())
-        {
-            if (directorio.mkdirs())
-            {
+        if (!directorio.exists()) {
+            if (directorio.mkdirs()) {
                 System.out.println("Directorio creado");
             }
-            else
-            {
+            else {
                 System.out.println("Error al crear directorio");
             }
         }
@@ -44,9 +39,11 @@ public class CreateBill {
         try {
             FileOutputStream file = new FileOutputStream(directorio.getPath()+"/bill" + info[13]+".pdf");
             Document doc = new Document();
+
             PdfWriter writer = PdfWriter.getInstance(doc, file);
             Image logotype = Image.getInstance("functionalities/administrador/Bill/src/assets/images/logotype.png");
             doc.open();
+
             logotype.setAlignment(Element.ALIGN_LEFT);
             logotype.scaleAbsolute(70, 70);
             doc.add(logotype);
@@ -56,6 +53,7 @@ public class CreateBill {
             BaseFont bf = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
             pb.setFontAndSize(bf, 12);
             pb.beginText();
+
             // X, Y de abajo hacia arriba e izquierda a derecha
             pb.setTextMatrix(20, 730);
             pb.showText("......................................................................................."
@@ -67,6 +65,7 @@ public class CreateBill {
             doc.add(new Paragraph("Fecha expedición: " + actualDate));
             doc.add(new Paragraph("Factura de venta No: " + info[0]));
             pb.setFontAndSize(bf, 7);
+
             if(plan == 5){
                 pb.setTextMatrix(315, 190);
                 pb.showText("Raja telecomunicaciones, tipos de planes:");
@@ -100,7 +99,8 @@ public class CreateBill {
                 pb.showText("El cobro por mora será del 1 % del precio de su plan, después de dos");
                 pb.setTextMatrix(315,70);
                 pb.showText("meses de mora el servicio de la línea será suspendido");
-            } else{
+            }
+            else{
                 pb.setTextMatrix(40, 190);
                 pb.showText("Raja telecomunicaciones, tipos de planes:");
                 pb.setTextMatrix(40, 182);
@@ -135,98 +135,131 @@ public class CreateBill {
                 pb.showText("meses de mora el servicio de la línea será suspendido");
             }
 
-
             pb.setFontAndSize(bf, 12);
             doc.add(Chunk.NEWLINE);
+
             Paragraph table = new Paragraph();
             PdfPTable tabla = new PdfPTable(5);
             pb.setColorFill(BaseColor.WHITE);
+
             PdfPCell cell = new PdfPCell(new Phrase("Factura mes: "));
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setBorderColor(BaseColor.WHITE);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(month));
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setBorderColor(BaseColor.WHITE);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("        "));
             cell.setBorder(Rectangle.NO_BORDER);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Número para pagos: "));
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setBorderColor(BaseColor.WHITE);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(info[17]));
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setBorderColor(BaseColor.WHITE);
+
             tabla.addCell(cell);
             tabla.setWidthPercentage(90);
+
             table.add(tabla);
             table.setIndentationLeft(-32);
+
             doc.add(table);
             doc.add(Chunk.NEWLINE);
+
             table = new Paragraph();
+
             tabla = new PdfPTable(5);
+
             cell = new PdfPCell(new Phrase("Límite de pago: "));
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setBorderColor(BaseColor.WHITE);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(cutDate));
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setBorderColor(BaseColor.WHITE);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("        "));
             cell.setBorder(Rectangle.NO_BORDER);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("TOTAL A PAGAR: "));
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setBorderColor(BaseColor.WHITE);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(info[11]));
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setBorderColor(BaseColor.WHITE);
+
             tabla.addCell(cell);
             tabla.setWidthPercentage(90);
+
             table.add(tabla);
             table.setIndentationLeft(-32);
+
             doc.add(table);
             doc.add(Chunk.NEWLINE);
+
             table = new Paragraph();
+
             tabla = new PdfPTable(1);
+
             cell = new PdfPCell(new Phrase("Estimado cliente, le recomendamos estar al tiempo con sus pagos para evitar el pago por mora "
                     + "o la suspensión del servicio, su servicio es del plan " + info[22] + " cuyo cobro por mora es de " + (Integer.parseInt(price[0]) * 0.01) + ". Si el pago ya fue realizado "
                     + "haga caso omiso."));
+
             cell.setBorder(Rectangle.NO_BORDER);
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
+
             tabla.addCell(cell);
             tabla.setWidthPercentage(98);
             table.setIndentationLeft(-12);
             table.add(tabla);
+
             doc.add(table);
             doc.add(Chunk.NEWLINE);
+
             table = new Paragraph();
+
             tabla = new PdfPTable(1);
+
             cell = new PdfPCell(new Phrase("RESUMEN DE LA CUENTA"));
             cell.setBorder(Rectangle.NO_BORDER);
             cell.setBackgroundColor(BaseColor.DARK_GRAY);
             cell.setHorizontalAlignment(1);
+
             tabla.addCell(cell);
             tabla.setWidthPercentage(98);
             table.setIndentationLeft(-12);
             table.add(tabla);
+
             doc.add(table);
             pb.setColorFill(BaseColor.BLACK);
+
             this.Graphic(info, plan, price);
             Image graphic = Image.getInstance("functionalities/administrador/Bill/src/assets/images/consumoComun.png");
+
             graphic.setAlignment(Element.ALIGN_LEFT);
             graphic.scaleAbsolute(200, 150);
             doc.add(graphic);
+
             if(plan == 4){
                 graphic = Image.getInstance("functionalities/administrador/Bill/src/assets/images/consumoAdicionalesplus.png");
                 graphic.setAbsolutePosition(310, 227);
                 graphic.scaleAbsolute(200, 150);
                 doc.add(graphic);
-            } else if(plan == 5){
+            }
+            else if(plan == 5){
                 graphic = Image.getInstance("functionalities/administrador/Bill/src/assets/images/consumoAdicionalesplus.png");
                 graphic.setAbsolutePosition(310, 227);
                 graphic.scaleAbsolute(200, 150);
@@ -235,7 +268,8 @@ public class CreateBill {
                 graphic.setAlignment(Element.ALIGN_LEFT);
                 graphic.scaleAbsolute(200, 150);
                 doc.add(graphic);
-            } else{
+            }
+            else{
                 graphic = Image.getInstance("functionalities/administrador/Bill/src/assets/images/consumoAdicionales.png");
                 graphic.setAbsolutePosition(310, 227);
                 graphic.scaleAbsolute(200, 150);
@@ -274,7 +308,8 @@ public class CreateBill {
                 data_waze = 2 * Integer.parseInt(price[7])-data_waze;
                 minutes_international = 2 * Integer.parseInt(price[8])-minutes_international;
                 data_shared = 2 * Integer.parseInt(price[9])-data_shared;
-            } else{
+            }
+            else{
                 data_consumption = Integer.parseInt(price[2])-data_consumption;
                 minutes_consumption =Integer.parseInt(price[1])-minutes_consumption;
                 sms_consumption = Integer.parseInt(price[3])-sms_consumption;
@@ -285,62 +320,88 @@ public class CreateBill {
                 minutes_international = Integer.parseInt(price[8])-minutes_international;
                 data_shared = Integer.parseInt(price[9])-data_shared;
             }
+
             doc.add(Chunk.NEWLINE);
             pb.setTextMatrix(30, 700);
             pb.showText("Tabla de consumo: ");
+
             tabla = new PdfPTable(4);
+
             cell = new PdfPCell(new Phrase("Consumo datos"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(data_consumption)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Consumo minutos"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(minutes_consumption)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Consumo mensajes"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(sms_consumption)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Consumo datos Whatsapp"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(data_wpp)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Consumo datos Facebook"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(data_fb)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Consumo minutos Whatsapp"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(minutes_wpp)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Consumo datos Waze"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(data_waze)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Consumo minutos internacionales"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(minutes_international)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Consumo datos compartidos"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(data_shared)));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("        "));
             cell.setBorder(Rectangle.NO_BORDER);
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("        "));
             cell.setBorder(Rectangle.NO_BORDER);
+
             tabla.addCell(cell);
             tabla.setWidthPercentage(98);
+
             doc.add(tabla);
             doc.add(Chunk.NEWLINE);
 
             Image promotion = Image.getInstance("functionalities/administrador/Bill/src/assets/images/raja_promocion.png");
+
             promotion.scaleAbsolute(500, 200);
             promotion.setAbsolutePosition(40,330);
             doc.add(promotion);
 
             Image footer = Image.getInstance("functionalities/administrador/Bill/src/assets/images/footer.png");
+
             footer.scaleAbsolute(500,120);
             footer.setAbsolutePosition(40, 210);
             doc.add(footer);
@@ -390,15 +451,18 @@ public class CreateBill {
           
             pb.addImage(logotype);
             pb.endText();
+
             Barcode128 code128 = new Barcode128();
             code128.setCode(StringDefault.trim());
             code128.setCodeType(Barcode128.CODE128);
+
             Image code128Image = code128.createImageWithBarcode(cb, null, null);
             code128Image.setAbsolutePosition(40, 80);
 
             code128Image.scaleAbsolute(400, 85);
             doc.add(code128Image);
             this.QRCode(info[17]);
+
             graphic = Image.getInstance("functionalities/administrador/Bill/src/assets/images/QrCode.png");
 
             graphic.setAbsolutePosition(460, 90);
@@ -411,17 +475,24 @@ public class CreateBill {
             doc.add(Chunk.NEWLINE);
             doc.add(Chunk.NEWLINE);
             doc.add(Chunk.NEWLINE);
+
             tabla = new PdfPTable(6);
+
             cell = new PdfPCell(new Phrase("Cliente"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(String.valueOf(info[20])));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Linea"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(info[19]));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase("Valor"));
             tabla.addCell(cell);
+
             cell = new PdfPCell(new Phrase(info[11]));
             tabla.addCell(cell);
             tabla.setWidthPercentage(98);
@@ -431,6 +502,7 @@ public class CreateBill {
         } catch (Exception e) {
             System.out.println("Something went wrong " + e);
         }
+
         return directorio.getPath();
     }
 
@@ -447,5 +519,4 @@ public class CreateBill {
             System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
         }
     }
-
 }
--- functionalities/administrador/Bill/src/CreateGraphic.java
+++ functionalities/administrador/Bill/src/CreateGraphic.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 
 public class CreateGraphic {
 
-
     public CreateGraphic(String[] consume, int idPlan, String[] price) {
         DefaultCategoryDataset dataset = createDataset(consume,1,3, price);
         JFreeChart chart = createChart(dataset, "Consumo mensual comun", 1);
@@ -21,23 +20,31 @@ public class CreateGraphic {
         if(idPlan == 4){
             dataset = createDataset(consume,4,7, price);
             JFreeChart  chart2 = createChart(dataset, "Consumo mensual adicionales", 3);
+
             ChartPanel chartPanel = new ChartPanel(chart2);
             chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        } else if(idPlan == 5){
+        }
+        else if(idPlan == 5){
             dataset = createDataset(consume,4,7, price);
             JFreeChart  chart2 = createChart(dataset, "Consumo mensual adicionales", 3);
+
             ChartPanel chartPanel = new ChartPanel(chart2);
             chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+
             dataset = createDataset(consume,8,9, price);
             JFreeChart  chart3 = createChart(dataset, "Consumo mensual internacional y compartido", 4);
+
             chartPanel = new ChartPanel(chart3);
             chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        } else {
+        }
+        else {
             dataset = createDataset(consume,4,6, price);
             JFreeChart chart1 = createChart(dataset, "Consumo mensual adicionales", 2);
+
             ChartPanel chartPanel = new ChartPanel(chart1);
             chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
         }
+
         // Put graph on a panel
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
@@ -47,22 +54,28 @@ public class CreateGraphic {
         DefaultCategoryDataset result = new DefaultCategoryDataset();
         String[] types = {"Consumo datos (MB)","Consumo minutos","Consumo mensajes","Consumo datos Whatsapp", "Consumo minutos Whatsapp",
                 "Consumo datos Facebook","Consumo datos Waze","Consumo minutos internaciones","Consumo datos compartidos"};
+
         for(int i=start; i<= end; i++){
             float value=0;
             if(Integer.parseInt(consume[12]) != 0){
                 if(i == 1){
                     value = 2 * Float.parseFloat(price[2]) - Float.parseFloat(consume[1]);
-                } else if(i == 2){
+                }
+                else if(i == 2){
                     value = 2 * Float.parseFloat(price[1]) - Float.parseFloat(consume[2]);
-                } else{
+                }
+                else{
                     value = 2 * Float.parseFloat(price[i]) - Float.parseFloat(consume[i]);
                 }
-            } else {
+            }
+            else {
                 if(i == 1){
                     value = Float.parseFloat(price[2]) - Float.parseFloat(consume[1]);
-                } else if(i == 2){
+                }
+                else if(i == 2){
                     value = Float.parseFloat(price[1]) - Float.parseFloat(consume[2]);
-                } else{
+                }
+                else{
                     value = Float.parseFloat(price[i]) - Float.parseFloat(consume[i]);
                 }
             }
@@ -88,12 +101,15 @@ public class CreateGraphic {
                 tipo = "internacionalycompartido";
                 break;
         }
+
         JFreeChart chart = ChartFactory.createBarChart3D(title, "Tipo",
                 "Consumo", dataset, // data
                 PlotOrientation.VERTICAL, true, // include legend
                 true, false);
+
         CategoryPlot plot = (CategoryPlot) chart.getPlot();
         CategoryAxis xAxis = (CategoryAxis) plot.getDomainAxis();
+
         xAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_45);
         plot.setBackgroundAlpha(0.5f);
         try {
@@ -101,7 +117,7 @@ public class CreateGraphic {
         } catch (IOException e) {
             System.out.println("Can't generate bill");
         }
-        return chart;
 
+        return chart;
     }
 }
